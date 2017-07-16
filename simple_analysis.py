053b059c456adc3be5cd2f7903c48786d65701ff
MilesCranmer <miles.cranmer@gmail.com>
2017-07-16 16:20:26 -0400

    Add more features

--- simple_analysis.py
+++ simple_analysis.py
@@ -2,7 +2,17 @@ import csv
 
 FEATURES = {
     'coffee': lambda array, index: array[index]['Coffee (mg caffeine)'],
-    'coffee-1': lambda array, index: array[index-1]['Coffee (mg caffeine)']
+    'coffee-1': lambda array, index: array[index-1]['Coffee (mg caffeine)'],
+    'coffee-2': lambda array, index: array[index-2]['Coffee (mg caffeine)'],
+    'coffee-1:5': lambda array, index: np.average([array[index-i]['Coffee (mg caffeine)'] for i in range(1, 6)]),
+    'water': lambda array, index: array[index]['Water (cups)'],
+    'water-1': lambda array, index: array[index-1]['Water (cups)'],
+    'water-2': lambda array, index: array[index-2]['Water (cups)'],
+    'water-1:5': lambda array, index: np.average([array[index-i]['Water (cups)'] for i in range(1, 6)]),
+    'sleep-1': lambda array, index: array[index-1]['Sleep'],
+    'sleep-2': lambda array, index: array[index-2]['Sleep'],
+    'sleep-3': lambda array, index: array[index-3]['Sleep'],
+    'sleep-1:5': lambda array, index: np.average([array[index-i]['Sleep'] for i in range(1, 6)])
 }
 
 with open('data/stats.csv') as datafile:
